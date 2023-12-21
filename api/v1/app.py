dd4d33d2caf737222143ef496d1a0c210e991e8e
jGohan-cpu <jjoseriveram01@gmail.com>
2023-12-21 13:26:19 -0500

    13th exercise fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-"""app.py"""
+"""Script to start the API"""
+
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask, render_template, make_response, jsonify
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 
-# Create a variable app, instance of Flask
+
 app = Flask(__name__)
 
-# Register the blueprint app_views to your Flask instance app
 app.register_blueprint(app_views)
 
 
-# Declare method to handle @app.teardown_appcontext that calls storage.close()
+# Create a CORS instance with permissive settings (for demonstration purposes)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Close the storage on teardown."""
+def close_db(exception):
+    """Close storage"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Error handler for a JSON-formatted 404 status code response."""
-    return jsonify({'error': 'Not found'}), 404
+def not_found(exception):
+    """404 error"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
-# Inside if __name__ == "__main__":, run your Flask server (variable app) with:
 if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = environ.get('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
+    """Main"""
+    app.run(host='0.0.0.0', port=5000)
