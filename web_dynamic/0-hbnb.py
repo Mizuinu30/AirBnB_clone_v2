68157e2dbf62e476b428e9dc09032ed8cfe383e0
Mizuinu30 <juansilva.dvm@gmail.com>
2024-02-08 15:15:04 -0400

    updated files

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -8,8 +8,8 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
-app = Flask(__name__)
 
+app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -20,7 +20,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/0-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -36,10 +36,11 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id = uuid.uuid4())
+                           places=places,
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,17 @@
-$('document').ready(function () {
-  let amenities = {};
-  $('INPUT[type="checkbox"]').change(function () {
-    if ($(this).is(':checked')) {
-      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else {
-      delete amenities[$(this).attr('data-id')];
-    }
-    $('.amenities H4').text(Object.values(amenities).join(', '));
+#!/usr/bin/node
+$(document).ready(function () {
+    let checkedAmenities = {};
+    $(document).on('change', "input[type='checkbox']", function () {
+      if (this.checked) {
+        checkedAmenities[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete checkedAmenities[$(this).data('id')];
+      }
+      let lst = Object.values(checkedAmenities);
+      if (lst.length > 0) {
+        $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
+      } else {
+        $('div.amenities > h4').html('&nbsp;');
+      }
+    });
   });
-});
