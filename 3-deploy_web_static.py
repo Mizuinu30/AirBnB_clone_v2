237dd5fd18ca7e18a56bc5ab2fdc3f63f6dcc305
Mizuinu30 <juansilva.dvm@gmail.com>
2024-02-16 13:10:38 -0400

    Commented correctly the code

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
+"""Deploy web static
 """
 
 from fabric.api import env, local, put, run
